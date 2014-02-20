f0ba4769099e723152ba0233fb6356181433d518
szymanski.krzysztof@gmail.com <szymanski.krzysztof@gmail.com@f3ea92f2-a43f-4da1-a5c3-affff0ab8db3>
2014-02-20 07:19:03 +0000

    added Sketchup directory utility class for handling platform specific directory locations. Fixed profiles tool for macosx. Replaced path +"\\"+ string concatenation with platform independent File.join

--- Plugins/Phlatboyz/tools/ProfilesTool.rb
+++ Plugins/Phlatboyz/tools/ProfilesTool.rb
@@ -1,96 +1,100 @@
-
 require 'Phlatboyz/PhlatTool.rb'
+require 'Phlatboyz/utils/SketchupDirectoryUtils.rb'
+
 # $Id$
 module PhlatScript
+  def PhlatScript.conformat(inp)
+    #replace mm with .mm
+    if PhlatScript.isMetric
+      out= inp.to_mm.to_s + '.mm'
+    else
+      #replace trailing " with .inch
+      out=inp.to_inch.to_s + '.inch'
+    end
+    out=out.gsub("~ ",'')
+    return out
+  end #
 
-   def PhlatScript.conformat(inp)
-      #replace mm with .mm
-	   if PhlatScript.isMetric
-         out= inp.to_mm.to_s + '.mm'
-	   else
-         #replace trailing " with .inch
-         out=inp.to_inch.to_s + '.inch'
-      end   
-      out=out.gsub("~ ",'')
-      return out
-   end #
+  class ProfilesSaveTool < PhlatTool
+    def initialize
+      @tooltype=(PB_MENU_MENU)
+      @tooltip="Save tool profile"
+      @statusText="Save Tool Profile"
+      @menuItem="SaveProfile"
+      @menuText="Save profile"
+    end
 
-   class ProfilesSaveTool < PhlatTool
-   
-      def initialize
-         @tooltype=(PB_MENU_MENU)
-         @tooltip="Save tool profile"
-         @statusText="Save Tool Profile"
-         @menuItem="SaveProfile"
-         @menuText="Save profile"
-      end
+    def select
+      model=Sketchup.active_model
 
-      def select
-         model=Sketchup.active_model
+      # prompts
+      prompts=['Name']
+      defaults=['default']
+      # dropdown options can be added here
+      #         list=["henry|bob|susan"] #should give list of existing?
 
-         # prompts
-         prompts=['Name']
-         defaults=['default']
-         # dropdown options can be added here
-#         list=["henry|bob|susan"] #should give list of existing?
+      input=UI.inputbox(prompts, defaults, 'Save Tool Profile')
+      # input is nil if user cancelled
+      if (input)
+        profilename=input[0].to_s.gsub(/ /,'')
 
-         input=UI.inputbox(prompts, defaults, 'Save Tool Profile')
-         # input is nil if user cancelled
-         if (input)
-            profilename=input[0].to_s.gsub(/ /,'')
-            print "saving to #{profilename}\n"
-            path=ENV['APPDATA'] + "\\Sketchup"
-            if not File.exist?(path)
-               Dir.mkdir(path)
-            end
-            if File.exist?(path)
-               #create file
-               outf=File.new(path + '\\' + profilename + '.tpr',"w")
-               #write contents
-               #outf.print "module PhlatScript\n";
-               outf.print "prof_spindlespeed=" + PhlatScript.spindleSpeed.to_i.to_s  + "\n"
-               outf.print "prof_feedrate="     + PhlatScript.conformat(PhlatScript.feedRate)  + "\n"
-               outf.print "prof_plungerate="   + PhlatScript.conformat(PhlatScript.plungeRate)  + "\n"
-               if (Profile_save_material_thickness)
-                  outf.print "prof_matthick="   + PhlatScript.conformat(PhlatScript.materialThickness)  + "\n"
-               end
-               outf.print "prof_cutfactor="    + PhlatScript.cutFactor.to_s   + "\n"
-               outf.print "prof_bitdiameter="  + PhlatScript.conformat(PhlatScript.bitDiameter)   + "\n"
-               outf.print "prof_tabwidth="     + PhlatScript.conformat(PhlatScript.tabWidth)   + "\n"
-               outf.print "prof_tabdepth="     + PhlatScript.tabDepth.to_i.to_s   + "\n"
-               outf.print "prof_safetravel="   + PhlatScript.conformat(PhlatScript.safeTravel)   + "\n"
-  
-               outf.print "prof_usemultipass=" + (PhlatScript.useMultipass? ? '1' : '0')   + "\n"
-               outf.print "prof_multipassdepth=" + PhlatScript.conformat(PhlatScript.multipassDepth)   + "\n"
-               outf.print "prof_gen3d="        + (PhlatScript.gen3D ? '1' : '0')   + "\n"
-               outf.print "prof_stepover="     + PhlatScript.stepover.to_i.to_s   + "\n"
-               #outf.print "end\n";
-               #close file
-               outf.close
-            else
-               print "ERROR path does not exist #{path}"
-            end # path exists so we saved it
-         end # if input
-      end # def select
-   end # class
+        path = SketchupDirectoryUtils.toolsProfilesPath()
 
-   class ProfilesLoadTool < PhlatTool
-      def initialize
-         @tooltype=(PB_MENU_MENU)
-         @tooltip="Load tool profile"
-         @statusText="Load Tool Profile"
-         @menuItem="LoadProfile"
-         @menuText="Load Profile"
-      end
+        if not File.exist?(path)
+          Dir.mkdir(path)
+        end
 
-      def select
-         model=Sketchup.active_model
+        print "saving to #{profilename}\n"
 
-         # prompts
-         prompts=["Select Profile"]
+        if File.exist?(path)
+          #create file
+          filePath = File.join(path, profilename + '.tpr')
+          outf=File.new(filePath,"w")
+          #write contents
+          #outf.print "module PhlatScript\n";
+          outf.print "prof_spindlespeed=" + PhlatScript.spindleSpeed.to_i.to_s  + "\n"
+          outf.print "prof_feedrate="     + PhlatScript.conformat(PhlatScript.feedRate)  + "\n"
+          outf.print "prof_plungerate="   + PhlatScript.conformat(PhlatScript.plungeRate)  + "\n"
+          if (Profile_save_material_thickness)
+            outf.print "prof_matthick="   + PhlatScript.conformat(PhlatScript.materialThickness)  + "\n"
+          end
+          outf.print "prof_cutfactor="    + PhlatScript.cutFactor.to_s   + "\n"
+          outf.print "prof_bitdiameter="  + PhlatScript.conformat(PhlatScript.bitDiameter)   + "\n"
+          outf.print "prof_tabwidth="     + PhlatScript.conformat(PhlatScript.tabWidth)   + "\n"
+          outf.print "prof_tabdepth="     + PhlatScript.tabDepth.to_i.to_s   + "\n"
+          outf.print "prof_safetravel="   + PhlatScript.conformat(PhlatScript.safeTravel)   + "\n"
 
-         defaults=['default']
-=begin         
+          outf.print "prof_usemultipass=" + (PhlatScript.useMultipass? ? '1' : '0')   + "\n"
+          outf.print "prof_multipassdepth=" + PhlatScript.conformat(PhlatScript.multipassDepth)   + "\n"
+          outf.print "prof_gen3d="        + (PhlatScript.gen3D ? '1' : '0')   + "\n"
+          outf.print "prof_stepover="     + PhlatScript.stepover.to_i.to_s   + "\n"
+          #outf.print "end\n";
+          #close file
+          outf.close
+        else
+          print "ERROR path does not exist #{path}"
+        end # path exists so we saved it
+      end # if input
+    end # def select
+  end # class
+
+  class ProfilesLoadTool < PhlatTool
+    def initialize
+      @tooltype=(PB_MENU_MENU)
+      @tooltip="Load tool profile"
+      @statusText="Load Tool Profile"
+      @menuItem="LoadProfile"
+      @menuText="Load Profile"
+    end
+
+    def select
+      model=Sketchup.active_model
+
+      # prompts
+      prompts=["Select Profile"]
+
+      defaults=['default']
+=begin
          defaults=[PhlatScript.spindleSpeed.to_s,
          Sketchup.format_length(PhlatScript.feedRate),
          Sketchup.format_length(PhlatScript.plungeRate),
@@ -113,112 +117,111 @@ module PhlatScript
          defaults.push(PhlatScript.showGplot?.inspect())
          defaults.push(encoded_comment_text)
 =end
-         # dropdown options can be added here
-         list=[""]
-         path=ENV['APPDATA'] + "\\Sketchup"
-         if not File.exist?(path)
-            Dir.mkdir(path)
-         end
-         if File.exist?(path)
-            #print "path exists\n"
-            Dir.foreach( path ) {| filename | 
-               #puts "got #{filename}"
-               if filename.index('.rb') or filename.index('.tpr')
-                  filename=filename.gsub(/\.rb|\.tpr/,"")
-                  if list[0] == ""
-                     list=[filename]
-                  else
-                     list[0]= list[0] + "|#{filename}"
-                  end
-               end
-               }
-#            puts list
-         end
-         if list[0] == ""
-            UI.messagebox('No profiles found. You have to save a profile before you can load one')
-            return
-         end
-
-         input=UI.inputbox(prompts, defaults, list, 'Load Profile')
-         # input is nil if user cancelled
-         if (input)
-            toget=input[0] + ".tpr"  # select tpr before rb
-            pth= path + "\\" + toget
-            if not File.exist?(pth)
-               toget=input[0] + ".rb"
-               pth= path + "\\" + toget
-            end
-
-            # load and interpret the file, creating variables
-            
-            inf = IO.readlines(pth)
-            if inf
-               inf.each { |line|
-#                  puts line 
-                  bits = line.split('=')
-                  #puts "bits[1] #{bits[1]}"
-                  
-                  if bits[1].index('.mm')
-                     value = bits[1].gsub('.mm','').to_f / 25.4    # to_l does not get it right when drawing is metric
-                     #puts "mm to inch #{value}"
-                  else
-                     if bits[1].index('.inch')
-                        value = bits[1].gsub('.inch','').to_f
-                     else
-                        value = bits[1].to_f
-                     end
-                  end
-               
-                  #puts "final value string " + value.to_s
-                  case bits[0]
-                     when 'prof_spindlespeed'
-                        PhlatScript.spindleSpeed = value
-                     when 'prof_feedrate'
-                        PhlatScript.feedRate = value.to_f
-                     when 'prof_plungerate'
-                        PhlatScript.plungeRate = value.to_f
-                     when 'prof_cutfactor'
-                        PhlatScript.cutFactor = value
-                     when 'prof_matthick'
-                        PhlatScript.materialThickness = value
-                     when 'prof_bitdiameter'
-                        PhlatScript.bitDiameter = value
-                     when 'prof_tabwidth'
-                        PhlatScript.tabWidth = value
-                     when 'prof_tabdepth'
-                        PhlatScript.tabDepth = value
-                     when 'prof_safetravel'
-                        PhlatScript.safeTravel = value
-                     when 'prof_usemultipass'
-                        PhlatScript.useMultipass = value > 0 ? true :  false
-                     when 'prof_multipassdepth'
-                        PhlatScript.multipassDepth = value
-                     when 'prof_gen3d'
-                        PhlatScript.gen3D = value > 0 ? true : false
-                     when 'prof_stepover'
-                        PhlatScript.stepover = value
-                     else
-                        puts "Unknown variable in load"
-                  end
-                  }
-               PhlatScript.commentText = "Loaded profile #{input[0]}"
-               puts "Loaded profile '#{input[0]}'"
+      # dropdown options can be added here
+      list=[""]
+      path = SketchupDirectoryUtils.toolsProfilesPath()
+      if not File.exist?(path)
+        Dir.mkdir(path)
+      end
+      if File.exist?(path)
+        #print "path exists\n"
+        Dir.foreach( path ) {| filename |
+          #puts "got #{filename}"
+          if filename.index('.rb') or filename.index('.tpr')
+            filename=filename.gsub(/\.rb|\.tpr/,"")
+            if list[0] == ""
+              list=[filename]
             else
-               puts "ERROR reading file for load"
+              list[0]= list[0] + "|#{filename}"
             end
-            
-            
+          end
+        }
+        #            puts list
+      end
+      if list[0] == ""
+        UI.messagebox('No profiles found. You have to save a profile before you can load one')
+        return
+      end
+
+      input=UI.inputbox(prompts, defaults, list, 'Load Profile')
+      # input is nil if user cancelled
+      if (input)
+        fileNameToOpen = input[0] + ".tpr"  # select tpr before rb
+        filePath = File.join(path , fileNameToOpen)
+        if not File.exist?(filePath)
+          fileNameToOpen = input[0] + ".rb"
+          filePath= File.join(path , fileNameToOpen)
+        end
+
+        # load and interpret the file, creating variables
+
+        inf = IO.readlines(filePath)
+        if inf
+          inf.each { |line|
+            #                  puts line
+            bits = line.split('=')
+            #puts "bits[1] #{bits[1]}"
+
+            if bits[1].index('.mm')
+              value = bits[1].gsub('.mm','').to_f / 25.4    # to_l does not get it right when drawing is metric
+              #puts "mm to inch #{value}"
+            else
+              if bits[1].index('.inch')
+                value = bits[1].gsub('.inch','').to_f
+              else
+                value = bits[1].to_f
+              end
+            end
+
+            #puts "final value string " + value.to_s
+            case bits[0]
+            when 'prof_spindlespeed'
+              PhlatScript.spindleSpeed = value
+            when 'prof_feedrate'
+              PhlatScript.feedRate = value.to_f
+            when 'prof_plungerate'
+              PhlatScript.plungeRate = value.to_f
+            when 'prof_cutfactor'
+              PhlatScript.cutFactor = value
+            when 'prof_matthick'
+              PhlatScript.materialThickness = value
+            when 'prof_bitdiameter'
+              PhlatScript.bitDiameter = value
+            when 'prof_tabwidth'
+              PhlatScript.tabWidth = value
+            when 'prof_tabdepth'
+              PhlatScript.tabDepth = value
+            when 'prof_safetravel'
+              PhlatScript.safeTravel = value
+            when 'prof_usemultipass'
+              PhlatScript.useMultipass = value > 0 ? true :  false
+            when 'prof_multipassdepth'
+              PhlatScript.multipassDepth = value
+            when 'prof_gen3d'
+              PhlatScript.gen3D = value > 0 ? true : false
+            when 'prof_stepover'
+              PhlatScript.stepover = value
+            else
+              puts "Unknown variable in load"
+            end
+          }
+          PhlatScript.commentText = "Loaded profile #{input[0]}"
+          puts "Loaded profile '#{input[0]}'"
+        else
+          puts "ERROR reading file for load"
+        end
+
 =begin
-            if Sketchup.load(pth) 
+            if Sketchup.load(pth)
                PhlatScript.spindleSpeed  =$prof_spindlespeed
-               PhlatScript.feedRate      =$prof_feedrate   
+               PhlatScript.feedRate      =$prof_feedrate
                PhlatScript.plungeRate    =$prof_plungerate
                PhlatScript.cutFactor     =$prof_cutfactor
                PhlatScript.bitDiameter   =$prof_bitdiameter
                PhlatScript.tabWidth      =$prof_tabwidth
                PhlatScript.tabDepth      =$prof_tabdepth
                PhlatScript.safeTravel    =$prof_safetravel
-  
+
                PhlatScript.useMultipass  =$prof_usemultipass
                PhlatScript.multipassDepth=$prof_multipassdepth
                PhlatScript.gen3D         =$prof_gen3d
@@ -226,77 +229,76 @@ module PhlatScript
             else
                puts "ERROR loading profile\n"
             end
-=end            
-         end # if input
-      end # def select
-   end # class
+=end
+      end # if input
+    end # def select
+  end # class
 
-   class ProfilesDeleteTool < PhlatTool
-      def initialize
-         @tooltype=(PB_MENU_MENU)
-         @tooltip="Delete tool profile"
-         @statusText="Delete Tool Profile"
-         @menuItem="DelProfile"
-         @menuText="Delete Profile"
+  class ProfilesDeleteTool < PhlatTool
+    def initialize
+      @tooltype=(PB_MENU_MENU)
+      @tooltip="Delete tool profile"
+      @statusText="Delete Tool Profile"
+      @menuItem="DelProfile"
+      @menuText="Delete Profile"
+    end
+
+    def select
+      model=Sketchup.active_model
+
+      # prompts
+      prompts=["Select Profile to Delete"]
+
+      defaults=['select one']
+      # dropdown options can be added here
+      list=[""]
+      path=ENV['APPDATA'] + "\\Sketchup"
+      if not File.exist?(path)
+        Dir.mkdir(path)
+      end
+      if File.exist?(path)
+        #print "path exists\n"
+        Dir.foreach( path ) {| filename |
+          #puts "got #{filename}"
+          if filename.index('.rb') or filename.index('.tpr')
+            filename=filename.gsub(/\.rb|\.tpr/,"")
+            if list[0] == ""
+              list=[filename]
+            else
+              list[0]= list[0] + "|#{filename}"
+            end
+          end
+        }
+        #            puts list
+      end
+      if list[0] == ""
+        UI.messagebox('No profiles found. You have to save a profile before you can delete one')
+        return
       end
 
-      def select
-         model=Sketchup.active_model
+      input=UI.inputbox(prompts, defaults, list, 'Delete Profile')
+      # input is nil if user cancelled
+      if (input)
+        toget=input[0] + ".rb"     # delete rb before tpr
+        pth= path + "\\" + toget
+        if not File.exist?(pth)
+          toget=input[0] + ".tpr"
+          pth= path + "\\" + toget
+        end
+        # delete the file
+        if File.exist?(pth)
+          if File.delete(pth)
+            UI.messagebox("Deleted the profile #{input[0]}")
+          else
+            UI.messagebox("FAILED to delete the profile #{input[0]}")
+          end
+        else
+          UI.messagebox('Profile does not exist for delete')
+          puts "delete: not found #{pth}"
+        end
 
-         # prompts
-         prompts=["Select Profile to Delete"]
+      end # if input
+    end # def select
+  end # class
 
-         defaults=['select one']
-         # dropdown options can be added here
-         list=[""]
-         path=ENV['APPDATA'] + "\\Sketchup"
-         if not File.exist?(path)
-            Dir.mkdir(path)
-         end
-         if File.exist?(path)
-            #print "path exists\n"
-            Dir.foreach( path ) {| filename | 
-               #puts "got #{filename}"
-               if filename.index('.rb') or filename.index('.tpr')
-                  filename=filename.gsub(/\.rb|\.tpr/,"")
-                  if list[0] == ""
-                     list=[filename]
-                  else
-                     list[0]= list[0] + "|#{filename}"
-                  end
-               end
-               }
-#            puts list
-         end
-         if list[0] == ""
-            UI.messagebox('No profiles found. You have to save a profile before you can delete one')
-            return
-         end
-
-         input=UI.inputbox(prompts, defaults, list, 'Delete Profile')
-         # input is nil if user cancelled
-         if (input)
-            toget=input[0] + ".rb"     # delete rb before tpr
-            pth= path + "\\" + toget
-            if not File.exist?(pth)
-               toget=input[0] + ".tpr"
-               pth= path + "\\" + toget
-            end
-            # delete the file
-            if File.exist?(pth)
-               if File.delete(pth)   
-                  UI.messagebox("Deleted the profile #{input[0]}")
-               else
-                  UI.messagebox("FAILED to delete the profile #{input[0]}")
-               end
-            else
-               UI.messagebox('Profile does not exist for delete')
-               puts "delete: not found #{pth}"
-            end
-            
-         end # if input
-      end # def select
-   end # class
-   
-   
 end #module
