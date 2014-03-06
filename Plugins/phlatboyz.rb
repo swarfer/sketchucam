638e1df53eee4444edcf3dd36a7a876e2fe224b3
szymanski.krzysztof@gmail.com <szymanski.krzysztof@gmail.com@f3ea92f2-a43f-4da1-a5c3-affff0ab8db3>
2014-03-06 07:29:48 +0000

    rolled back platboyzStrings changes

--- Plugins/phlatboyz.rb
+++ Plugins/phlatboyz.rb
@@ -60,6 +60,8 @@ module PhlatScript
       end
    end # if version
 
+   @@phlatboyzStrings = LanguageHandler.new('PhlatBoyz.strings')
+   
    class PhlatScriptExtention < SketchupExtension 
      def initialize
        super 'Phlatboyz Tools', 'Phlatboyz/Phlatscript.rb' 
