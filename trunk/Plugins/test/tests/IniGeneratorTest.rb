15e61cd65541121d490bd8a7e19b236fdbcf9f02
szymanski.krzysztof@gmail.com <szymanski.krzysztof@gmail.com@f3ea92f2-a43f-4da1-a5c3-affff0ab8db3>
2014-03-08 17:55:33 +0000

    simplified IniGenerator, added more tests

--- trunk/Plugins/test/tests/IniGeneratorTest.rb
+++ trunk/Plugins/test/tests/IniGeneratorTest.rb
@@ -1,7 +1,9 @@
 module PhlatScript
   require_relative 'TestBase'
   require_relative '../../Phlatboyz/utils/IniParser'
+
   class IniGeneratorTest < TestBase
+
     def run()
       generator = IniGenerator.new()
 
@@ -11,7 +13,7 @@ module PhlatScript
       assertException("should raise exception for incorrect parameter") {
         generator.hashMapToIni({"ss"=>"xx"});
       }
-      
+
       assertTrue(generator.isSimpleType("a"),"should be a simple type")
       assertTrue(generator.isSimpleType(1),"should be a simple type")
       assertTrue(generator.isSimpleType(1.0),"should be a simple type")
@@ -19,10 +21,27 @@ module PhlatScript
       assertFalse(generator.isSimpleType({"d"=>"d"}),"should be a complex type")
       map = {"section"=>{"a"=>"b"}}
       result = generator.hashMapToIni({"section"=>{"a"=>"b"}});
+      
+        
       assertEquals(result,"[section]\na=b\n","should be equal")
+
+      map = {"section"=>{"a"=>1}}
+      result = generator.hashMapToIni(map);  
+      assertEquals(result,"[section]\na=1\n","should be equal")
+
+      
+      map = {"section"=>{"a"=>1.1}}
+      result = generator.hashMapToIni(map);  
+      assertEquals(result,"[section]\na=1.1\n","should be equal")
+
+      map = {"section"=>{"a"=>"1.1mm"}}
+      result = generator.hashMapToIni(map);  
+      assertEquals(result,"[section]\na=1.1mm\n","should be equal")
+      
       generator.dumpHashMapToIni(map,"__test_file_map.ini")
       assertTrue((File.exist?"__test_file_map.ini"), "file needs to exist after dump");
       File.delete("__test_file_map.ini")
+
     end
   end
 end
