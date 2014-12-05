30f061159291d635312c7c0d1d64764efe988d09
swarfer@gmail.com <swarfer@gmail.com@f3ea92f2-a43f-4da1-a5c3-affff0ab8db3>
2014-12-05 09:35:12 +0000

    gcode joiner initial version done 1.2a3

--- trunk/Plugins/Phlatboyz/tools/PhJoiner.rb
+++ trunk/Plugins/Phlatboyz/tools/PhJoiner.rb
@@ -20,14 +20,82 @@ module PhlatScript
       end
 
       def select
-         UI.messagebox("test joiner",MB_MULTILINE)
       
-         #output first file till 'G0 X0 Y0 (home)'
-         #output file 2 to N-1 from 'G90 G21 G49 G61' till 'G0 X0 Y0 (home)'
-         #output file N from 'G90 G21 G49 G61' till end
+   # get multuiple file names
+         directory_name = PhlatScript.cncFileDir
+         filename = PhlatScript.cncFileName
+         status = false
+         filenames = Array.new
+         wildcard = "Select|\*.#{$phoptions.default_file_ext}||"
+         result = UI.openpanel("Select first gcode file", directory_name, wildcard)
+         while (result != nil)
+            filenames += [result]
+            result = UI.openpanel("Select next Gcode file, cancel to end", directory_name, wildcard)
+         end
+
+         if filenames.length <= 1
+            UI.messagebox("Not enough files selected, exiting")
+            return
+         end
+         
+         #get output file name
+         UI.messagebox("Now you will be promted for the output file name")
+         outputfile = UI.savepanel("Select output file name", directory_name, "joined#{$phoptions.default_file_ext}" )
+         if (outputfile == nil)
+            UI.messagebox("No output file selected, exiting")
+            return
+         end
+         
+         outf = File.new(outputfile, "w+")
+         
+         idx = 0
+         lastfile = filenames.length - 1  #last file
+         while (idx < filenames.length)
+            puts "output file #{idx}"
+            inf = File.new(filenames[idx],"r")
+            #output first file till 'G0 X0 Y0 (home)'
+            #output file 2 to N-1 from 'G90 G21 G49 G61' till 'G0 X0 Y0 (home)'
+            #output file N from 'G90 G21 G49 G61' till end
+            if (idx > 0)  # then skip header
+               line = inf.readline
+               while (line.match('G90') == nil)
+                  if (line.match('\(File:|\(Bit') != nil)
+                     outf.puts(line)
+                  end
+                  line = inf.readline
+               end
+               puts "header skipped #{idx}"
+               outf.puts(line)
+            end
+            #output till footer
+            line = inf.readline
+            while !inf.eof and (line.match('G0 X0 Y0|M30|\(Outfeed\)') == nil)
+               outf.puts(line)   if !line.match('\(Outfeed|M30')
+               line = inf.readline
+            end
+            puts "output till footer done #{idx}  #{line}"
+            
+            if (idx == lastfile)
+               #output footer
+               puts "writing footer #{idx}"
+               outf.puts(line)
+               while !inf.eof
+                  line = inf.readline
+                  outf.puts(line)
+               end #while
+            end
+            
+            inf.close
+            puts "closed #{idx}"
+            idx += 1
+         end # while   
+         outf.close
+         puts "finished writing joined files"
+         if (UI.messagebox("All files joined into file #{outputfile}, do you want to preview the Gcode?",MB_YESNO) == IDYES)
+            GPlot.new.plot(outputfile)
+         end
        
-       
-        end #select
+      end #select
    end # class
 
 end # module
