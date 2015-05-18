a9af6664ae118d9fefe61e59d65e57062e10404f
swarfer <swarfer@gmail.com>
2015-05-18 09:10:04 +0100

    get make.bat to put the git version number in the filename
    put the abbreviated git version number in the filename

--- make.bat
+++ make.bat
@@ -10,6 +10,17 @@ goto zipit
 
 :cdo
    c:
+REM stole this from git-cmd.bat, should set git path for us so we don't have to run gitshell
+   @setlocal
+
+   @rem Get the absolute path to the current directory, which is assumed to be the
+   @rem Git installation root.
+   @set git_install_root=C:\Users\david\AppData\Local\GitHub\PortableGit_c2ba306e536fdf878271f7fe636a147ff37326ad
+   @set PATH=%git_install_root%\bin;%git_install_root%\mingw\bin;%git_install_root%\cmd;%PATH%
+
+   @set PLINK_PROTOCOL=ssh
+   @if not defined TERM set TERM=msys
+   
    if exist "C:\Program Files (x86)\Google\Google SketchUp 8" cd "C:\Program Files (x86)\Google\Google SketchUp 8\Plugins"
    if exist "C:\Program Files\Google\Google SketchUp 8\Plugins" cd "C:\Program Files\Google\Google SketchUp 8\Plugins"
    if exist Phlatboyz goto zipit
@@ -22,8 +33,8 @@ goto zipit
 rem   del ..\sketchucam-1*.rbz
    cd ..
 REM   subwcrev .\ phrev.txt phrev.dat     // git does not support this
-   echo a > phrev.dat
-   php move.php plugins\tp.zip SketchUcam-1_3.rbz
+   git describe --abbrev=4 --dirty --always --tags > phrev.dat
+   php move.php plugins\tp.zip SketchUcam-1_3a.rbz
 
 goto end
 
