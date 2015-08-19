fa5573d93a02fb9fc12f58ffad30c641213bd9f5
swarfer <swarfer@gmail.com>
2015-08-19 15:51:41 +0100

    csink icons and fix for phlatten on V >= 2014
    phlatten was not flattening on Sketchup 2015/2014

--- Plugins/Phlatboyz/tools/PhlattenTool.rb
+++ Plugins/Phlatboyz/tools/PhlattenTool.rb
@@ -23,28 +23,33 @@ module PhlatScript
       faces = []
       Sketchup.active_model.start_operation "Phlattening selected edges", true
       Sketchup.active_model.selection.each { |entity| 
-        edges.push(entity) if entity.kind_of?(Sketchup::Edge)  
-        entity.erase! if entity.kind_of?(Sketchup::Face)
-      }
+         edges.push(entity) if entity.kind_of?(Sketchup::Edge)  
+         faces.push(entity) if entity.kind_of?(Sketchup::Face)
+         }
+         # for Sketchup > 2014 we have to delete the faces seperately
+      faces.each { |entity| 
+         entity.erase! if entity.kind_of?(Sketchup::Face)
+         }
+         
       Sketchup.active_model.selection.clear
-
       edges.each { |edge|
-        edge.faces.each { |face|
-          face.erase! if face.valid?
-        }
-        pos1 = edge.vertices.first.position
-        pos2 = edge.vertices.last.position
-        pt1 = Geom::Point3d.new(pos1.x, pos1.y, 0)
-        pt2 = Geom::Point3d.new(pos2.x, pos2.y, 0)
-        del_edges.push(edge)
-        new_edges.push([pt1,pt2])
-      }
+         edge.faces.each { |face|
+            face.erase! if face.valid?
+            }
+         pos1 = edge.vertices.first.position
+         pos2 = edge.vertices.last.position
+         pt1 = Geom::Point3d.new(pos1.x, pos1.y, 0)
+         pt2 = Geom::Point3d.new(pos2.x, pos2.y, 0)
+         del_edges.push(edge)
+         new_edges.push([pt1,pt2])
+         }
 
-      Sketchup.active_model.entities.erase_entities del_edges
+      Sketchup.active_model.entities.erase_entities(del_edges)
+
       new_edges.each { |pts| 
-        edges = Sketchup.active_model.entities.add_edges(pts[0], pts[1])
-        edges.each { |e| e.find_faces} if edges
-      }
+         edges = Sketchup.active_model.entities.add_edges(pts[0], pts[1])
+         edges.each {|e| e.find_faces} if edges
+         }
 
       Sketchup.active_model.commit_operation
     end
