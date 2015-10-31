25cef771f86ee05d5c6ca483ae36437054880ed7
David the Swarfer <swarfer@gmail.com>
2015-10-31 06:18:37 +0200

    bump version to 1.4 in make

--- make.bat
+++ make.bat
@@ -35,7 +35,7 @@ rem   del ..\sketchucam-1*.rbz
    cd ..
 REM   subwcrev .\ phrev.txt phrev.dat     // git does not support this
    
-   php move.php plugins\tp.zip SketchUcam-1_3b.rbz
+   php move.php plugins\tp.zip SketchUcam-1_4.rbz
 
 goto end
 
