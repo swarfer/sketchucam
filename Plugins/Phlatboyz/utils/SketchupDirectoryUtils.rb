cebef9c0aa7271f24f89c8ccf52d65b05e11ecf5
David the Swarfer <swarfer@gmail.com>
2017-01-06 17:06:44 +0200

    fix for Mac profile save bug -
    The old savepanel bug is still active in mac versions.
    must not give savepanel a wildcard filename

--- Plugins/Phlatboyz/utils/SketchupDirectoryUtils.rb
+++ Plugins/Phlatboyz/utils/SketchupDirectoryUtils.rb
@@ -6,21 +6,29 @@ module PhlatScript
 #   class SketchupDirectoryUtils
 # not a class, just a tool method as part of PhlatScript. module
 
-     def PhlatScript.toolsProfilesPath
-       # this method returns a path for storing profiles.
-       # path depends on the system, macosx is recognized by RUBY_PLATFORM.
-       if RUBY_PLATFORM =~ /mswin/
-         # Windows
-         path = ENV['APPDATA'] + "\\Sketchup"
-       else #MAC
+   def PhlatScript.isMac?
+      if (RUBY_PLATFORM =~ /mswin|mingw/)
+         return false
+      else
+         return true
+      end 
+   end
+
+   def PhlatScript.toolsProfilesPath
+      # this method returns a path for storing profiles.
+      # path depends on the system, macosx is recognized by RUBY_PLATFORM.
+      if PhlatScript.isMac?
+         #MAC
          # Sketchup.find_support_file returns directory in:
          # ~/Library/Application Support/Sketchup X/SketchUp/
          # we will store profiles in Profiles subdirectory
          path = File.join(Sketchup.find_support_file("Plugins"),"Phlatboyz", "Profiles")
-
-       end
-       return path
-     end
+      else
+         # Windows - support files are read only in win7, so use appdata
+         path = ENV['APPDATA'] + "\\Sketchup"
+      end
+      return path
+   end
 #   end
 
 end
